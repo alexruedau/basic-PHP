08fc3bf7ab6efb5e190e6e9cad735004c7ec5707
Alex Rueda <alexruedau@gmail.com>
2019-01-02 11:41:36 +0100

    Ejercicio #2 Recorrer arreglos que contienen a su vez arreglos

--- ejercicio-1.php
+++ ejercicio-1.php
@@ -1,14 +1,11 @@
 <?php
 echo "<h2>Ejercicio 1</h2>";
 $arreglo = [
-
-	'keyStr1' => 'lado',
+  'keyStr1' => 'lado',
 	0 => 'ledo',
-
-	'keyStr2' => 'lido',
+  'keyStr2' => 'lido',
 	1 => 'lodo',
 	2 => 'ludo'
-
 ];
 foreach ($arreglo as $elementoASC) {
 	echo $elementoASC . ', ';
@@ -21,5 +18,19 @@ foreach ($arreglo as $elementoDESC) {
 }
 echo "<br> ¡Qué trabajo me ha costado! <br>";
 
+echo "<br><h2>Ejercicio 2</h2>";
 
+$paises = [
+  'Francia' => ['Paris', 'Lille', 'Bordeaux'],
+	'Alemania' => ['Berlín', 'Bremen', 'Düsseldorf']
+];
+
+
+foreach ($paises as $pais=>$ciudades) {
+  echo '<b>' . $pais . '</b>: ';
+  foreach ($ciudades as $ciudad) {
+    echo $ciudad . ', ';
+  }
+  echo "<br>";
+}
 ?>
