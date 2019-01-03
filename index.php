21251d71ae629533df1785a55f58d75d0c2253d2
Alex Rueda <alexruedau@gmail.com>
2019-01-03 17:34:15 +0100

    Agregando archivos externos en PHP. Se cambió de lugar el arreglo y las funciones de index.php a jobs.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+include('jobs.php'); //se puede hacer solo un llamado
+//require('jobs.php');//se pueden hacer los llamados que sean necesarios
+//require_once('jobs.php');//solo un llamado
 $lastName = 'Rueda U.';
 $name = 'Alex ' . $lastName;
 $limitMonths = 1000;
@@ -10,63 +14,7 @@ var_dump($name);*/
   2 => 'DevOps'
 ];*/
 
-// Arreglos dentro de arreglos
-$jobs = [
-  [
-    'title' => 'PHP Developer',
-    'description' => 'Éste es un gran trabajo!',
-    'visible' => true,
-    'months' => 12
-  ],
-  [
-    'title' => 'Python Dev',
-    'visible' => true,
-    'months' => 40
-  ],
-  [
-    'title' => 'DevOps',
-    'visible' => true,
-    'months' => 80
-  ],
-  [
-    'title' => 'Node Dev',
-    'visible' => true,
-    'months' => 20
-  ],
-  [
-    'title' => 'Frontend Dev',
-    'visible' => true,
-    'months' => 70
-  ]
-];
-function getDuration($months){
-  $years = floor($months/12);
-  $extraMonths = $months % 12;
-    if ($years == 0){
-      return "$extraMonths months";
-    } elseif ($extraMonths == 0) {
-      return "$years years";
-    } else
-    return "$years years $extraMonths months";
-}
 
-function printJob($job){
-  if ($job['visible'] == false){
-    return;
-  }
-
-  echo '<li class="work-position">';
-    echo '<h5>' . $job['title'] . '</h5>';
-    echo '<p>' . $job['description'] . '</p>';
-    echo '<p>' . getDuration($job['months']) . '</p>';
-    echo '<strong>Achievements:</strong>';
-    echo '<ul>';
-      echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-      echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-      echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-    echo '</ul>';
-  echo '</li>';
-}
 
 ?>
 
