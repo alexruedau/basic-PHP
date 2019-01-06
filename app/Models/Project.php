f359df0de45930db7b0227b1b2222f9b241b0269
Alex Rueda <alexruedau@gmail.com>
2019-01-06 02:27:54 +0100

    Agregando la parte de Projects utilizando Eloquent (tal cual como en Jobs)

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
-require_once 'BaseElement.php';
-
-class Project extends BaseElement{
-
+class Project extends Model{
+  protected $table = 'projects';
 }
 
 ?>
--- jobs.php
+++ jobs.php
@@ -3,11 +3,8 @@
 use App\Models\{Job, Project};
 
 $jobs = Job::all();
+$projects = Project::all();
 
-$project1 = new Project('Project 1', 'Description #1');
-$projects = [
-  $project1
-];
 
 function printElement($job){
   /*if ($job->visible == false){
