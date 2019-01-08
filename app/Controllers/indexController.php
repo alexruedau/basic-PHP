566dd7dc126a89d566e5a68f341a504b67511309
Alex Rueda <alexruedau@gmail.com>
2019-01-08 01:05:03 +0100

    Templates con Twig parte 2 ... Utilizando la sintaxis correcta en index.twig

--- app/Controllers/indexController.php
+++ app/Controllers/indexController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Models\{Job, Project};
 
-class IndexController {
+class IndexController extends BaseController{
   public function indexAction(){
 
     $jobs = Job::all();
@@ -16,7 +16,10 @@ class IndexController {
     $name = 'Alex ' . $lastName;
     $limitMonths = 1000;
 
-    include '../views/index.php';
+    return $this->renderHTML('index.twig', [
+      'name' => $name,
+      'jobs' => $jobs
+    ]);
 
   }
 }
