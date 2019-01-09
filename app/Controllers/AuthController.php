0421b05647a28df36b28e8b6db12b6560f1a515a
Alex Rueda <alexruedau@gmail.com>
2019-01-09 19:53:03 +0100

    Mejorando Formulario de Login

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\User;
 use Respect\Validation\Validator as v;
+use Zend\Diactoros\Response\RedirectResponse;
 
 class AuthController extends BaseController {
   public function getLogin(){
@@ -11,20 +12,25 @@ class AuthController extends BaseController {
   }
 
   public function postLogin($request){
+    $postData = $request->getParsedBody();
+    $responseMessage = null;
 
-      $postData = $request->getParsedBody();
-      $user = User::where('email', $postData['email'])->first();
-      if ($user){
-        if (\password_verify($postData['password'], $user->password)) {
-          echo 'Correct';
-        } else {
-          echo 'Wrong';
-        }
+    $user = User::where('email', $postData['email'])->first();
+    if ($user){
+      if (\password_verify($postData['password'], $user->password)) {
+        return new RedirectResponse('admin');
       } else {
-        echo 'Not found';
+        $responseMessage = 'Bad credentials';
       }
+    } else {
+      $responseMessage = 'Bad credentials';
+    }
 
-   }
+    return $this->renderHTML('login.twig', [
+      'responseMessage' => $responseMessage
+    ]);
+
+  }
 
 }
 
